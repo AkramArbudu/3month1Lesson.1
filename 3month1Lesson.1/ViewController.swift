6c374962ecfb4db29d21997624a66d7e68e5a481
arbudu akram <arbudu02@g>
2022-11-19 19:50:40 +0600

    дз 3 мес 1 урок

--- 3month1Lesson.1/ViewController.swift
+++ 3month1Lesson.1/ViewController.swift
@@ -9,8 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstView: UIView!
+    
+    @IBOutlet weak var secondView: UIView!
+    
+    @IBOutlet weak var thirdView: UIView!
+    
+    @IBOutlet weak var fourView: UIView!
+    
+    @IBOutlet weak var firstImageView: UIImageView!
+    
+    @IBOutlet weak var secondImageView: UIImageView!
+    
+    @IBOutlet weak var thirdImageView: UIImageView!
+    
+    @IBOutlet weak var fourImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        firstView.layer.cornerRadius = 30
+        secondView.layer.cornerRadius = 30
+        thirdView.layer.cornerRadius = 30
+        fourView.layer.cornerRadius = 30
+        firstImageView.layer.cornerRadius = 85
+        secondImageView.layer.cornerRadius = 85
+        thirdImageView.layer.cornerRadius = 55
+        fourImageView.layer.cornerRadius = 85
         // Do any additional setup after loading the view.
     }
 
